77fba18732eac55de57abb9248bc136bebc495fb
Estefania del Castillo Medrano <ecasmed@gmail.com>
2019-11-05 16:35:37 +0100

    create-obstacles

--- js/game.js
+++ js/game.js
@@ -35,11 +35,11 @@ const Game = {
             this.moveAll();
 
 
-            // this.clearObstacles()
-            // if (this.framesCounter % 70 === 0) this.generateObstacles()
-            // if (this.framesCounter % 100 === 0) this.score++;
+            this.clearObstacles()
+            if (this.framesCounter % 70 === 0) this.generateObstacles()
+            if (this.framesCounter % 100 === 0) this.score++;
             // if (this.isCollision()) this.gameOver()
-            // if (this.framesCounter > 1000) this.framesCounter = 0;
+            if (this.framesCounter > 1000) this.framesCounter = 0;
         }, 1000 / this.fps)
     },
 
@@ -47,7 +47,7 @@ const Game = {
     reset: function () {
         this.background = new Background(this.ctx, this.width, this.height);
         this.player = new Player(this.ctx, 50, 150, './images/my-fairis (1).png', this.width, this.height, this.playerKeys);
-        // this.obstacles = [];
+        this.obstacles = [];
         ScoreBoard.init(this.ctx, this.score)
     },
 
@@ -59,33 +59,33 @@ const Game = {
     drawAll: function () {
         this.background.draw();
         this.player.draw(this.framesCounter);
-        // this.obstacles.forEach(obstacle => obstacle.draw())
+        this.obstacles.forEach(obstacle => obstacle.draw())
         ScoreBoard.draw(this.score)
     },
 
     moveAll: function () {
         this.background.move()
         this.player.move()
-        // this.obstacles.forEach(obstacle => obstacle.move())
+        this.obstacles.forEach(obstacle => obstacle.move())
 
     },
 
-    // generateObstacles: function () {
-    //     this.obstacles.push(new Obstacle(this.ctx, 15, 45, this.width, this.height))
-    // },
+    generateObstacles: function () {
+        this.obstacles.push(new Obstacle(this.ctx, 50, 50, this.width, Math.floor(Math.random() * (800 - 200) + 200)))
+    },
 
-    // gameOver: function () {
-    //     clearInterval(this.interval)
-    // },
-
-
-    // isCollision: function () {
-    //     return this.obstacles.some(obs => (this.player.posX + this.player.width > obs.posX && obs.posX + obs.width > this.player.posX && this.player.posY + this.player.height > obs.posY && obs.posY + obs.height > this.player.posY))
-    // },
+    gameOver: function () {
+        clearInterval(this.interval)
+    },
 
 
-    // clearObstacles: function () {
-    //     this.obstacles = this.obstacles.filter(obstacle => (obstacle.posX >= 0))
-    // }
+    isCollision: function () {
+        return this.obstacles.some(obs => (this.player.posX + this.player.width > obs.posX && obs.posX + obs.width > this.player.posX && this.player.posY + this.player.height > obs.posY && obs.posY + obs.height > this.player.posY))
+    },
+
+
+    clearObstacles: function () {
+        this.obstacles = this.obstacles.filter(obstacle => (obstacle.posX >= 0))
+    }
 
 }
--- js/obstacle.js
+++ js/obstacle.js
@@ -4,20 +4,50 @@ class Obstacle {
         this.width = width;
         this.height = height;
 
-        this.imageObstacle = new Image()
-        this.imageObstacle.src = "./images/my-fairis (1).png"
+        this.image = new Image()
+        this.image.src = "./images/output-onlinejpgtools (1).png"
         this.posX = gameWidth;
-        this.posY = gameHeight * 98 - this.height;
+        // this.posY = gameHeight * 98 - this.height;
 
-        this.vx = 10;
+
+        this.posY = gameHeight * 1 - this.height;
+        this.posY0 = gameHeight * 1 - this.height;
+
+        this.frames = 5;
+        this.framesIndex = 0;
+
+        this.vx = 5;
     }
 
 
-    draw() {
-        
+    draw(framesCounter) {
+        this.ctx.drawImage(
+            //this.image, this.posX, this.posY, this.width, this.height)
+            this.image,
+            this.framesIndex * Math.floor(this.image.width / this.frames),
+            0,
+            Math.floor(this.image.width / this.frames),
+            this.image.height,
+            this.posX,
+            this.posY,
+            this.width,
+            this.height
+        )
+        this.animate(framesCounter)
+
     }
+
 
     move() {
         this.posX -= this.vx;
+    }
+
+
+    animate(framesCounter) {
+        if (framesCounter % 10 === 0) {
+            this.framesIndex++;
+
+            if (this.framesIndex > 4) this.framesIndex = 0;
+        }
     }
 }
